d3eb8bd15227aa45afda4e493034c7b375ad2a7d
Jakub Antonín Štigler <xstigl00@stud.fit.vut.cz>
2024-11-16 00:09:32 +0100

    Add missing data to mouse state

--- termal_core/src/raw/events/mouse.rs
+++ termal_core/src/raw/events/mouse.rs
@@ -1,16 +1,19 @@
 bitflags::bitflags! {
     #[doc = "Key modifiers. Some of them are usualy not sent to terminals."]
     #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
-    pub struct MouseState: u32 {
+    pub(crate) struct MouseState: u32 {
         const PRIMARY = 0x0;
         const MIDDLE = 0x1;
-        const SCROLL_DOWN = 0x1;
         const SECONDARY = 0x2;
         const RELEASE = 0x3;
         const BUTTON = 0x3;
         const SHIFT = 0x4;
         const ALT = 0x8;
         const CONTROL = 0x10;
-        const SCROLL = 0x20;
+        const MODIFIERS = 0x1C;
+        const MOVE = 0x20;
+        const SCROLL_UP = 0x40;
+        const SCROLL_DOWN = 0x41;
+        const ACTION = 0x43;
     }
 }
