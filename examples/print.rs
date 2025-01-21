21951d939ce4ff2c24713f8c4d1604d02d7006dd
Jakub Antonín Štigler <xstigl00@stud.fit.vut.cz>
2025-01-21 15:51:43 +0100

    Better print example

--- examples/print.rs
+++ examples/print.rs
@@ -1,7 +1,7 @@
 use termal::reset_terminal;
 
 fn main() {
-    println!("something hello there\x1b#3");
+    println!("\nsomething hello there\x1b#4");
     // _ = stdin().read_line(&mut String::new());
     reset_terminal();
 }
